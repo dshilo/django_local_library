ac0161a9a86c91f3ec4ada77b813c88681f6e010
Shilo <dror.shilo@intel.com>
2019-09-01 09:38:20 +0300

    add host

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -148,3 +148,6 @@ STATIC_URL = '/static/'
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+ALLOWED_HOSTS = ['whispering-wildwood-90399.herokuapp.com']
